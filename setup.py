705b31cca6fca1958f924931903bd1982719e281
Sam Bull <git@sambull.org>
2023-11-25 01:47:53 +0000

    Add framework

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        "Framework :: aiohttp",
     ],
     keywords=[
         'flashbag',
